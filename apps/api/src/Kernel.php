16a581efadcdae6ec8ba3b76952e2d339d60249f
adp1002 <alvarodel98@hotmail.com>
2023-02-06 21:49:52 +0100

    Added hexagonal architecture symfony app

---
+++ apps/api/src/Kernel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace DumpIt\Api;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+}
