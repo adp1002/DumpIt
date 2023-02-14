6f40322d1e116e2e9dd94281519d020fdcd37286
adp1002 <alvarodel98@hotmail.com>
2023-02-14 11:26:22 +0100

    Added xdebug and tweaks to docker

--- apps/api/config/bundles.php
+++ apps/api/config/bundles.php
@@ -2,6 +2,5 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
 ];
--- apps/api/src/Controller/TestController.php
+++ apps/api/src/Controller/TestController.php
@@ -6,9 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/api/test", name="test", methods={"GET"})
- */
+#[Route('/api/test', name: 'test', methods: 'GET')]
 class TestController extends AbstractController
 {
     public function __invoke(): JsonResponse
